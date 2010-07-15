536affe156f7e79b81fcc4697548e7363e2a6e90
mheily <mheily@9caf6951-243e-42fb-97d8-ad36769e700a>
2010-07-15 01:23:56 +0000

    bug

--- pthread_workqueue.c
+++ pthread_workqueue.c
@@ -56,7 +56,7 @@
 
 struct work {
     STAILQ_ENTRY(work)   item_entry; 
-    void              *(*func)(void *);
+    void               (*func)(void *);
     void                *func_arg;
     unsigned int         flags;
     unsigned int         gencount;
@@ -267,7 +267,7 @@ pthread_workqueue_create_np(pthread_workqueue_t *workqp,
 
 int
 pthread_workqueue_additem_np(pthread_workqueue_t workq,
-                     void *( *workitem_func)(void *), void * workitem_arg,
+                     void (*workitem_func)(void *), void * workitem_arg,
                      pthread_workitem_handle_t * itemhandlep, 
                      unsigned int *gencountp)
 {
--- pthread_workqueue.h
+++ pthread_workqueue.h
@@ -55,7 +55,7 @@ int pthread_workqueue_create_np(pthread_workqueue_t * workqp,
                const pthread_workqueue_attr_t * attr);
 
 int pthread_workqueue_additem_np(pthread_workqueue_t workq,
-            void *( *workitem_func)(void *), void * workitem_arg,
+            void (*workitem_func)(void *), void * workitem_arg,
             pthread_workitem_handle_t * itemhandlep, unsigned int *gencountp);
 
 int pthread_workqueue_attr_init_np(pthread_workqueue_attr_t * attrp);
