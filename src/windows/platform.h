6a773cdc1287c831968f802425df32c3c726df0c
mheily <mheily@9caf6951-243e-42fb-97d8-ad36769e700a>
2011-05-18 01:15:49 +0000

    Replace pthread emulation macros with winpthreads.h from http://locklessinc.com/articles/pthreads_on_windows/

--- src/windows/platform.h
+++ src/windows/platform.h
@@ -12,7 +12,7 @@
 
 #include <windows.h>
 #include <string.h>
-#include "pthread_cond.h"
+#include "winpthreads.h"
 
 /* Instead of __attribute__ ((constructor)), use DllMain() */
 #define CONSTRUCTOR	  /* */
@@ -27,30 +27,6 @@
 #define sleep(sec) 	Sleep(1000*sec)
 #define strdup(p)	_strdup(p)
 #define random()	rand()
-
-typedef HANDLE pthread_t;
-
-/* Emulation of pthreads mutex functionality */
-/* (copied from libkqueue) */
-#define PTHREAD_PROCESS_SHARED 1
-#define PTHREAD_PROCESS_PRIVATE 2
-typedef CRITICAL_SECTION pthread_mutex_t;
-typedef CRITICAL_SECTION pthread_spinlock_t;
-typedef CRITICAL_SECTION pthread_rwlock_t;
-#define _cs_init(x)  InitializeCriticalSection((x))
-#define _cs_lock(x)  EnterCriticalSection ((x))
-#define _cs_unlock(x)  LeaveCriticalSection ((x))
-#define pthread_mutex_lock _cs_lock
-#define pthread_mutex_unlock _cs_unlock
-#define pthread_mutex_init(x,y) _cs_init((x))
-#define pthread_spin_lock _cs_lock
-#define pthread_spin_unlock _cs_unlock
-#define pthread_spin_init(x,y) _cs_init((x))
-#define pthread_mutex_init(x,y) _cs_init((x))
-#define pthread_rwlock_rdlock _cs_lock
-#define pthread_rwlock_wrlock _cs_lock
-#define pthread_rwlock_unlock _cs_unlock
-#define pthread_rwlock_init(x,y) _cs_init((x))
 
 /* Specific workqueue items */
 #define WORKQUEUE_PLATFORM_SPECIFIC \
