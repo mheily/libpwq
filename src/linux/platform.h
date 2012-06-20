9b4dae6df627738d06d010b32802e4f23499990f
mheily <mheily@9caf6951-243e-42fb-97d8-ad36769e700a>
2012-06-20 19:05:25 +0000

    Workaround for lack of spinlocks on Android

--- src/linux/platform.h
+++ src/linux/platform.h
@@ -34,4 +34,16 @@
 
 unsigned int linux_get_runqueue_length(void);
 
+/* 
+ * Android does not provide spinlocks.
+ * See: http://code.google.com/p/android/issues/detail?id=21622
+ */
+#if defined(__ANDROID_API__)
+#define pthread_spinlock_t     pthread_mutex_t
+#define pthread_spin_lock      pthread_mutex_lock
+#define pthread_spin_unlock    pthread_mutex_unlock
+#define pthread_spin_init(a,b) pthread_mutex_init((a), NULL)
+#define pthread_spin_destroy   pthread_mutex_destroy
+#endif /* defined(__ANDROID_API__) */
+
 #endif /* _LIBPWQ_LINUX_PLATFORM_H */
